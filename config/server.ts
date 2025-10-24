59be39d91a108b75fe622e6c94d72a2a4838779b
Natalio-R <nataliorabasconavarro@gmail.com>
2025-10-24 15:30:20 +0200

    Upgrade connections

--- config/server.ts
+++ config/server.ts
@@ -4,5 +4,5 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://cms-strapi-ozwwntxvw-natalio-rabasco-navarros-projects.vercel.app/'),
+  url: env('PUBLIC_URL', 'https://cms-strapi-am.vercel.app/'),
 });
